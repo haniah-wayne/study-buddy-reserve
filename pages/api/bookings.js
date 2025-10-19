f6f6560b883156009fdf75c93bf8d2cd3d7f82a0
haniah-wayne <hl1263@wayne.edu>
2025-10-19 10:57:43 -0400

    Update bookings.js

--- pages/api/bookings.js
+++ pages/api/bookings.js
@@ -1,6 +1,7 @@
-export default function handler(req, res) {
+// pages/api/bookings.js
+export default function handler(_req, res) {
   const data = [
-    { id: 1, roomName: "Study Room 1104", building: "Undergraduate Library", date: "Sept 16, 2025", time: "10am-12pm" }
-  ];
-  res.status(200).json({ bookings: data });
+    { id: 1, roomName: 'Study Room 1104', building: 'Undergraduate Library', date: 'Sept 16, 2025', time: '10am-12pm' },
+  ]
+  res.status(200).json({ bookings: data })
 }
