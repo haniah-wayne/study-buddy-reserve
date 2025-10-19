e2374c92c5cfc9016e48c2206c6c6814a15d8058
haniah-wayne <hl1263@wayne.edu>
2025-10-19 10:55:46 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
-import '../styles/globals.css';
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// pages/_app.tsx
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
